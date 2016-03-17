0c6d044c1e2543a4ad6baf185fb41a6981542f31
Adnan Duric <Adnan.Duric@d2l.com>
2016-03-17 10:00:54 -0400

    Adds repo_prefs data model

---
+++ bot/src/repo_prefs/mod.rs
@@ -0,0 +1,13 @@
+pub struct RepoPrefsManager;
+
+impl RepoPrefsManager {
+    pub fn fetch_all {
+        
+    }
+    pub fn update {
+        
+    }
+    pub fn delete {
+        
+    }
+}
---
+++ bot/src/repo_prefs/model.rs
@@ -0,0 +1,41 @@
+use postgres::{self, Connection};
+
+#[derive(PartialEq,Eq)]
+pub struct RepoPrefsDataModel {
+    pub audience: String,
+    pub project: String,
+    pub repo: String
+}
+
+impl RepoPrefsDataModel {
+    pub fn initialize(conn: &Connection) -> Result<(), postgres::error::Error> {
+        try!(conn.execute("CREATE TABLE IF NOT EXISTS repo_prefs (
+            audience    VARCHAR(10),
+            project     VARCHAR(10),
+            repo        VARCHAR(100),
+            PRIMARY KEY (audience, project, repo)
+        )", &[]));
+        
+        Ok(())
+    }
+
+    pub fn all(conn: &Connection) -> Result<Vec<PullRequestDataModel>, postgres::error::Error> {
+        let statement = try!(conn.prepare("SELECT * FROM repo_prefs"));
+        let results = try!(statement.query(&[]));
+        Ok(results.iter().map(|r| RepoPrefsDataModel {
+            aud: r.get(0),
+            project: r.get(1),
+            repo: r.get(2)
+        }).collect::<Vec<_>>())
+    }
+
+    pub fn create(conn: &Connection, audience: &String, project: &String, repo: &String) -> Result<(), postgres::error::Error> {
+        try!(conn.execute("INSERT INTO repo_prefs (audience, project, repo) VALUES ($1, $2, $3)", &[audience, project, repo]));
+        Ok(())
+    }
+    
+    pub fn delete(conn: &Connection, audience: &String, project: &String, repo: &String) -> Result<(), postgres::error::Error> {
+        try!(conn.execute("DELETE FROM repo_prefs WHERE id = $1 AND project = $2 AND repo = $3", &[audience, project, repo]));
+        Ok(())
+    }
+}
